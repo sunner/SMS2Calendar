e13bc9a0d08a5719c77be739e4afe8e2d54236d4
Sunner Sun <sunner@gmail.com>
2016-06-29 12:39:35 +0800

    Add comments of interface

--- app/src/main/java/cn/sunner/autosmstocalendar/SMSParser.java
+++ app/src/main/java/cn/sunner/autosmstocalendar/SMSParser.java
@@ -6,5 +6,11 @@ import android.provider.CalendarContract;
  * Created by Sunner on 6/28/16.
  */
 public interface SMSParser {
+    /**
+     * Return the event extracted from text
+     *
+     * @param text SMS to parse
+     * @return return null when mismatch
+     */
     public CalendarContract.Events getEventFrom(String text);
 }
