e851dfecf7ce5dc9926eb44cd4c5d1360891267b
Sunner Sun <sunner@gmail.com>
2016-07-21 19:45:00 +0800

    Read SMS framwork

--- app/src/main/java/cn/sunner/sms2calendar/SMSReceiver.java
+++ app/src/main/java/cn/sunner/sms2calendar/SMSReceiver.java
@@ -3,15 +3,44 @@ package cn.sunner.sms2calendar;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
+    private String TAG = "SMS2Cal";
+
     public SMSReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Get the data (SMS data) bound to intent
+        Bundle bundle = intent.getExtras();
+
+        SmsMessage[] msgs = null;
+
+        String str = "";
+
+        if (bundle != null) {
+            // Retrieve the SMS Messages received
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+
+            // For every SMS message received
+            for (int i=0; i < msgs.length; i++) {
+                // Convert Object array
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                // Sender's phone number
+                str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
+                // Fetch the text message
+                str += msgs[i].getMessageBody().toString();
+                // Newline
+                str += "\n";
+            }
+
+            // Display the entire SMS Message
+            Log.d(TAG, str);
+        }
     }
 }
