5abfb0d7aeefb1a604d5715b85e482b31c26c459
Sunner Sun <sunner@gmail.com>
2016-07-27 09:24:29 +0800

    Make my number parser have super power

--- app/src/main/java/cn/sunner/sms2calendar/N15558132322Parser.java
+++ app/src/main/java/cn/sunner/sms2calendar/N15558132322Parser.java
@@ -1,10 +1,70 @@
 package cn.sunner.sms2calendar;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Calendar;
+
 /**
  * Created by Sunner on 7/26/16.
+ *
+ * I can use this class to test all kinds of patterns
+ * and send events to anybody. Especially to my home leader, ;-)
  */
-public class N15558132322Parser extends N106980000666Parser {
+public class N15558132322Parser extends SMSParser {
+    SMSParser realParser;
+
     public N15558132322Parser(String text) {
         super(text);
+    }
+
+    @Override
+    protected boolean parse() {
+
+        // Try all other parsers
+        try {
+            Class <?> [] parserClasses = {
+                    Class.forName("cn.sunner.sms2calendar.N12306Parser"),
+                    Class.forName("cn.sunner.sms2calendar.N106980000666Parser"),
+            };
+
+            for (Class <?> parserClass: parserClasses) {
+                realParser = (SMSParser) parserClass.getDeclaredConstructor(String.class).newInstance(text);
+                if (realParser != null && realParser.isValid()) {
+                    return true;
+                }
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public String getTitle() {
+        return realParser.getTitle();
+    }
+
+    @Override
+    public Calendar getBeginTime() {
+        return realParser.getBeginTime();
+    }
+
+    @Override
+    public Calendar getEndTime() {
+        return realParser.getEndTime();
+    }
+
+    @Override
+    public String getLocation() {
+        return realParser.getLocation();
+    }
+
+    @Override
+    public boolean isValid() {
+        return realParser.isValid();
+    }
+
+    @Override
+    public String getText() {
+        return realParser.getText();
     }
 }
---
+++ app/src/test/java/cn/sunner/sms2calendar/N15558132322ParserTest.java
@@ -0,0 +1,33 @@
+package cn.sunner.sms2calendar;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sunner on 7/27/16.
+ */
+public class N15558132322ParserTest {
+
+    @Test
+    public void test12306Parse() throws Exception {
+        SMSParser parser = new N15558132322Parser("订单EC11541789,孙先生您已购1月24日D2245次14车13F号南京南19:11开。【铁路客服】");
+        assertTrue(parser.isValid());
+        assertEquals("D2245次14车13F号", parser.getTitle());
+        assertEquals("南京南站", parser.getLocation());
+        assertEquals(new GregorianCalendar(Calendar.getInstance().get(Calendar.YEAR), 1, 24, 19, 11), parser.getBeginTime());
+    }
+
+    @Test
+    public void testCtripParse() throws Exception {
+        SMSParser parser = new N15558132322Parser("已出票：订单9987654321『首都航空 JD5688 杭州萧山T3-海口美兰 10月1日9:00-10月1日11:35 陈丽玮，票号898-1234567890；孙志岗，票号898-1234567890』请提前2小时至机场值机。可在APP『行程』频道查看行程，值机选座、退改点击http://t.ctrip.cn/dFxBvc【携程网】");
+        assertTrue(parser.isValid());
+        assertEquals("首都航空JD5688", parser.getTitle());
+        assertEquals("杭州萧山T3", parser.getLocation());
+        assertEquals(new GregorianCalendar(Calendar.getInstance().get(Calendar.YEAR), 10, 1, 9, 0), parser.getBeginTime());
+        assertEquals(new GregorianCalendar(Calendar.getInstance().get(Calendar.YEAR), 10, 1, 11, 35), parser.getEndTime());
+    }
+}
