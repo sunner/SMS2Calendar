bb195ff1fdb38d810834ecd5e8a29597ca804b5d
Sunner Sun <sunner@gmail.com>
2016-08-03 17:28:50 +0800

    Make a real main activity

--- app/src/main/java/cn/sunner/sms2calendar/MainActivity.java
+++ app/src/main/java/cn/sunner/sms2calendar/MainActivity.java
@@ -2,7 +2,6 @@ package cn.sunner.sms2calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void addOneNotification(View view){
-        SMSParser parser = new N12306Parser("订单EC11541789,孙先生您已购1月24日D2245次14车13F号南京南19:11开。【铁路客服】");
-        Notification.add(this, parser);
     }
 }
---
+++ app/src/main/java/cn/sunner/sms2calendar/TestActivity.java
@@ -0,0 +1,19 @@
+package cn.sunner.sms2calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void addOneNotification(View view){
+        SMSParser parser = new N12306Parser("订单EC11541789,孙先生您已购1月24日D2245次14车13F号南京南19:11开。【铁路客服】");
+        Notification.add(this, parser);
+    }
+}
