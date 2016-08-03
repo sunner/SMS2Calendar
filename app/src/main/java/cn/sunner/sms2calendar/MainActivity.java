59f09d0a51aac16915db9692165f911389218774
Sunner Sun <sunner@gmail.com>
2016-08-03 20:03:13 +0800

    Add permission request

--- app/src/main/java/cn/sunner/sms2calendar/MainActivity.java
+++ app/src/main/java/cn/sunner/sms2calendar/MainActivity.java
@@ -1,5 +1,9 @@
 package cn.sunner.sms2calendar;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Request permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECEIVE_SMS},
+                    0);
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
