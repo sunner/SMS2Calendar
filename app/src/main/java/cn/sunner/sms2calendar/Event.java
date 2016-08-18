3cb32c8783efe5cec5fb77c1bcf5c9eaf12e6145
Sunner Sun <sunner@gmail.com>
2016-08-18 21:24:28 +0800

    Fix null bugs

--- app/src/main/java/cn/sunner/sms2calendar/Event.java
+++ app/src/main/java/cn/sunner/sms2calendar/Event.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
 
 /**
  * Created by Sunner on 8/13/16.
- *
+ * <p>
  * Calendar events.
  */
 public class Event {
@@ -17,54 +17,66 @@ public class Event {
     protected Calendar endTime;
     protected String location = "";
 
-    public void setText(String text) {
-        this.text = text;
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setBeginTime(Calendar beginTime) {
-        this.beginTime = beginTime;
-    }
-
-    public void setEndTime(Calendar endTime) {
-        this.endTime = endTime;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
     public Calendar getBeginTime() {
         return beginTime;
+    }
+
+    public void setBeginTime(Calendar beginTime) {
+        this.beginTime = beginTime;
     }
 
     public Calendar getEndTime() {
         return endTime;
     }
 
+    public void setEndTime(Calendar endTime) {
+        this.endTime = endTime;
+    }
+
     public String getLocation() {
         return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public String getText() {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public String getFormattedText(Context context) {
         DateFormat dateFormat = DateFormat.getDateTimeInstance();
         String formattedText = "";
 
-        formattedText += context.getString(R.string.event) + ": " + getTitle() + "\n";
-        formattedText += context.getString(R.string.location) + ": " + getLocation() + "\n";
-        formattedText += context.getString(R.string.begin) + ": " + dateFormat.format(getBeginTime().getTime()) + "\n";
-        formattedText += context.getString(R.string.end) + ": " + dateFormat.format(getEndTime().getTime());
+        formattedText += String.format("%s: %s\n",
+                context.getString(R.string.event),
+                getTitle());
+        formattedText += String.format("%s: %s\n",
+                context.getString(R.string.location),
+                getLocation());
+        formattedText += String.format("%s: %s\n",
+                context.getString(R.string.begin),
+                (getEndTime() != null)
+                        ? dateFormat.format(getBeginTime().getTime())
+                        : context.getString(R.string.na));
+        formattedText += String.format("%s: %s",
+                context.getString(R.string.end),
+                (getEndTime() != null)
+                        ? dateFormat.format(getEndTime().getTime())
+                        : context.getString(R.string.na));
 
         return formattedText;
     }
